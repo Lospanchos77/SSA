ce0e87c2fa766fcb369c4f6913f917d5d725da1f
Lospanchos <118466179+Lospanchos77@users.noreply.github.com>
2025-05-16 16:15:35 +0200

    Commit 16/05/2025

--- obj/Debug/net9.0/cheeker.AssemblyInfo.cs
+++ obj/Debug/net9.0/cheeker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cheeker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428dfbfdc1dece4dfdab05a34f57072602c63bd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc5cf4abbd8758b849f7c7a714881f4a082cbc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("cheeker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cheeker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
